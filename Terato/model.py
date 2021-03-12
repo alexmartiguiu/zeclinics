e6438a7936d83e88ee5c47058c003d559ba2146a
elias <abad.elias00@gmail.com>
2021-03-12 12:17:49 +0100

    first

--- Terato/model.py
+++ Terato/model.py
@@ -12,6 +12,4 @@ def createDeepLabv3(outputchannels=6):
     model = models.segmentation.deeplabv3_resnet101(pretrained=True,
                                                     progress=True)
     model.classifier = DeepLabHead(2048, outputchannels)
-    # Set the model in training mode
-    model.train()
     return model
